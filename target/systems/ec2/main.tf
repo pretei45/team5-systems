645741cba524712a446fa62a11ddf8bef698dfed
pretei45 <127458451+pretei45@users.noreply.github.com>
2023-07-08 19:01:29 -0500

    Update main.tf

--- target/systems/ec2/main.tf
+++ target/systems/ec2/main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.7.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "server" {
+    source = "https://github.com/pretei45/base-iac/tree/main//modules/ec2"
+    num = 5
+}
